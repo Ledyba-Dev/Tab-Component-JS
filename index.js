be9d691b56dae276a326010452d7e3d6f7186e95
Rodri <rodrigovizcarra9623@gmail.com>
2024-10-25 14:44:26 -0300

    Codigo Finalizado

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+const dataTabFunction = (e) => {
+    const buttons = document.querySelectorAll("button[data-tab]");
+    const sections = document.querySelectorAll("section[data-tab]");
+
+    buttons.forEach((button) => button.setAttribute("data-active", "false"));
+    sections.forEach((section) => section.setAttribute("data-active", "false"));
+
+    e.target.setAttribute("data-active", "true");
+    const similarAtribute = e.target.getAttribute("data-tab");
+    const sectionToActivated = document.querySelector(
+        `section[data-tab=${similarAtribute}]`
+    );
+    sectionToActivated.setAttribute("data-active", "true");
+};
+
+document.addEventListener("click", (e) => {
+    if (e.target.matches("button[data-tab]")) {
+        dataTabFunction(e);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {});
